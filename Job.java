141f8e309b5a7904f3e64f118f89bc88b2ea3bfe
saida <sa1018@exeter.ac.uk>
2023-10-20 13:30:24 +0100

    added stuff

--- Job.java
+++ Job.java
@@ -1,21 +1,24 @@
-1 public class Job implements Runnable {
-2 private int jobNumber;
-3
-4 Job (int jobNumber) {
-5 this.jobNumber = jobNumber;
-6 }
-7
-8 public void run () {
-9 // Undertake required work, here we will emulate it by sleeping for a period
-10 System.out.println ("Job: " + jobNumber + " is being processed by thread : "
-11 + Thread.currentThread ().getName());
-12 try {
-13 Thread.sleep((int)(1000));
-14 } catch (InterruptedException e) {
-15 // no catching as example should not experience interruptions
-16 }
-17 System.out.println("Job: " + jobNumber + " is ending in thread : "
-18 + Thread.currentThread().getName());
-19 }
-20
-21 }
+
+public class Job implements Runnable {
+	
+	private int jobNumber;
+
+	//constructor for the class Job
+	Job (int jobNumber) {
+		this.jobNumber = jobNumber;
+	}
+
+	public void run () {
+		// Undertake required work, here we will emulate it by sleeping for a period
+		System.out.println ("Job: " + jobNumber + " is being processed by thread : "
+				+ Thread.currentThread ().getName());
+		try {
+			Thread.sleep((int)(1000));
+		} catch (InterruptedException e) {
+			// no catching as example should not experience interruptions
+		}
+		System.out.println("Job: " + jobNumber + " is ending in thread : "
+				+ Thread.currentThread().getName());
+}
+
+}
